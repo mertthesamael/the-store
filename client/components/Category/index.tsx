49a05bf2db89d9aa54943d1094244aefac1bd5ba
Merto <mertenercan@gmail.com>
2023-05-17 05:21:18 +0300

    - category section static refactors   - added category images staticly

--- client/components/Category/index.tsx
+++ client/components/Category/index.tsx
@@ -1,11 +1,12 @@
+import Image from "next/image"
 import styles from "./style.module.scss"
 
-
-const Category = () => {
+const Category = ({category,img}:{category:string, img:any}) => {
 
     return(
         <div className={styles.category}>
-
+            <Image src={img} style={{objectFit:'cover'}} fill alt="category banner"></Image>
+            <h2>{category}</h2>
         </div>
     )
 }
--- client/containers/home-categories/index.tsx
+++ client/containers/home-categories/index.tsx
@@ -1,6 +1,9 @@
 import Category from "@/components/Category";
 import styles from "./style.module.scss"
-
+import jump from '@/assets/png/jump.png'
+import cargo from '@/assets/png/cargo.png'
+import terror from '@/assets/png/terror.png'
+import spray from '@/assets/png/spray.png'
 
 const HomeCategories = () => {
 
@@ -13,10 +16,10 @@ const HomeCategories = () => {
             </div>
             <h1>Categories</h1>
             <div className={styles.categories__items}>
-                <Category></Category>
-                <Category></Category>
-                <Category></Category>
-                <Category></Category>
+                <Category category={"Shirt"} img={spray}></Category>
+                <Category category={"Hoodie"} img={terror}></Category>
+                <Category category={"Leggings"} img={cargo}></Category>
+                <Category category={"Shoe"} img={jump}></Category>
             </div>
         </section>
     )
